8deb2ddd34738e6984cb4d2dcf0d616410ae759a
soohyun <soohyun_kim2@tmax.co.kr>
2021-12-06 11:29:36 +0900

    initial commit with gradle

---
+++ note-api/src/main/java/wapl/noteapi/noteapi/NoteApiApplication.java
@@ -0,0 +1,13 @@
+package wapl.noteapi.noteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteApiApplication.class, args);
+	}
+
+}
