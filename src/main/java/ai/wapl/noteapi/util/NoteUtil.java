3f1fb8137f98d9a6340efc14ec431d20de827309
soohyun <soohyun_kim2@tmax.co.kr>
2021-12-23 18:02:47 +0900

    feat(note): chapter random gen color

--- src/main/java/ai/wapl/noteapi/util/NoteUtil.java
+++ src/main/java/ai/wapl/noteapi/util/NoteUtil.java
@@ -2,6 +2,7 @@ package ai.wapl.noteapi.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.TimeZone;
 
 public class NoteUtil {
@@ -13,5 +14,19 @@ public class NoteUtil {
         sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
 
         return sdf.format(time) + " Asia/Seoul";
+    }
+
+    public static String getRandomColor() {
+        String[] colorArray = {
+                "#C84847", "#F29274", "#F6C750",
+                "#77B69B", "#679886", "#3A7973",
+                "#77BED3", "#5C83DA", "#8F91E7",
+                "#DF97AA", "#CA6D6D"
+        };
+
+        int idx = new Random().nextInt(colorArray.length);
+        String randomColor = colorArray[idx];
+
+        return randomColor;
     }
 }
