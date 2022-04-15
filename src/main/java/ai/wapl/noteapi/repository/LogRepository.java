2ae7444cc00c543be20e71b66233286dec988b4c
hwieun <hwieun_kim@tmax.co.kr>
2022-04-15 17:42:02 +0900

    fix(note): new 표시 기간 15일 기획에 따른 쿼리 조건 추가

--- src/main/java/ai/wapl/noteapi/repository/LogRepository.java
+++ src/main/java/ai/wapl/noteapi/repository/LogRepository.java
@@ -16,10 +16,11 @@ public interface LogRepository extends JpaRepository<NoteLog, Long> {
       + "\t, row_number() over(partition by resource_id order by log_id desc) row_num\n"
       + "\tfrom tb_noteapp_log a\n"
       + "\twhere action in ('create', 'edit_done', 'rename') and \n"
+      + "\tlog_timestamp >= current_timestamp - interval '15' day and \n"
       + "\tresource_id in (select note_id from tb_noteapp_note_mst where parent_notebook = :chapterId)\n"
       + ") where row_num = 1\n"
       + ") b on b.resource_id = a.resource_id and a.log_id >= b.log_id\n"
-      + "where a.user_id = :userId", nativeQuery = true)
+      + "where a.user_id = :userId ", nativeQuery = true)
   Set<String> getReadListByChapterId(@Param("userId") String userId,
       @Param("chapterId") String chapterId);
 
@@ -29,6 +30,7 @@ public interface LogRepository extends JpaRepository<NoteLog, Long> {
       + "\t, row_number() over(partition by resource_id order by log_id desc) row_num\n"
       + "\tfrom tb_noteapp_log a\n"
       + "\twhere action in ('create', 'edit_done', 'rename') and \n"
+      + "\tlog_timestamp >= current_timestamp - interval '15' day and \n"
       + "\tresource_id in (select id from tb_noteapp_notebook_mst where note_channel_id = :channelId)\n"
       + ") where row_num = 1\n"
       + ") b on b.resource_id = a.resource_id and a.log_id >= b.log_id\n"
