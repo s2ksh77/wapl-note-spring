30d20fde25c9e72f29286653378feb78d8dcf806
soohyun <soohyun_kim2@tmax.co.kr>
2022-05-06 14:57:59 +0900

    chore(note): 누락된 dto 추가

---
+++ src/main/java/ai/wapl/noteapi/dto/TagDTOInterface.java
@@ -0,0 +1,11 @@
+package ai.wapl.noteapi.dto;
+
+public interface TagDTOInterface {
+    String getId();
+
+    String getName();
+
+ //    String getPageId(); // 이 후 필요할 경우
+
+    Integer getTagCount();
+}
